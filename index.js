053f8096779430d1b505fdfbb1de05246af2f3a6
Taylor Kirkes <kirkes21@gmail.com>
2022-03-05 17:24:29 -0600

    finish input js

--- index.js
+++ index.js
@@ -9,11 +9,35 @@ const gameState = {
 }
 
 // Listeners
-function getPlayerNames() {
+  // hide input fields
+function hideInputs() {
+  let nameInputs = document.getElementById("name-entry")
+  nameInputs.style.display = "none"
+}
+
+function get1PName() {
   let p1Name = document.getElementById('p1NameEntry').value
   document.getElementById('p1NameDisplay').innerText = p1Name
-  document.getElementById('p2NameEntry').value
+  document.getElementById('p2NameDisplay').innerText = 'Computer'
+
+  hideInputs()
+}
+
+function get2PNames() {
+  let p1Name = document.getElementById('p1NameEntry').value
+  document.getElementById('p1NameDisplay').innerText = p1Name
+  let p2Name = document.getElementById('p2NameEntry').value
   document.getElementById('p2NameDisplay').innerText = p2Name
+  
+  hideInputs()
+}
+    
+function start1PGame() {
+  get1PName()
+}
+
+function start2PGame() {
+  get2PNames()
 }
 
 // Render
@@ -21,3 +45,10 @@ function renderState() {
 
 }
 
+// Bring it on home
+function initGame() {
+
+}
+
+// Go for launch
+initGame()
