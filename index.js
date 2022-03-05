3c37e12a1c87cdb8d7efd444b42c737ae3305726
Taylor Kirkes <kirkes21@gmail.com>
2022-03-05 15:38:54 -0600

    reformat skeleton

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // state
 const gameState = {
   players: ['x', 'o'],
-  
+  p1Score: 0,
+  p2Score: 0,
+  p1Name: '',
+  p2Name: '',
+  status: 'Enter name(s) and choose a mode.'
 }
 
 function buildInitialState() {
@@ -13,26 +17,3 @@ function renderState() {
 
 }
 
-// maybe a dozen or so helper functions for tiny pieces of the interface
-
-// listeners
-function onBoardClick() {
-  // update state, maybe with another dozen or so helper functions...
-
-  renderState() // show the user the new state
-}
-const board = document.getElementById('board');
-board.addEventListener('click', onBoardClick); // etc
-
-function tick() {
-    // this is an incremental change that happens to the state every time you update...
-  
-    renderState()
-  }
-  
-  // now you might have things like
-  document.addEventListener('keydown', function (event) {
-    // here you might read which key was pressed and update the state accordingly
-  })
-
-  
